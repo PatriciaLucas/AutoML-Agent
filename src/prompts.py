6a9705fda002e900433d462eec7d708c1b46543f
PATRÍCIA DE OLIVEIRA E LUCAS <39201177+PatriciaLucas@users.noreply.github.com>
2025-09-03 10:18:03 -0300

    Update prompts.py

--- src/prompts.py
+++ src/prompts.py
@@ -3,35 +3,38 @@ class Prompts:
 
     def get_prompt(name: str, **kwargs) -> str:
         match name:
-            case 'Etapa 1': # Imputação de valores faltantes: 
+            case 'Etapa 1': # Imputação de valores faltantes:
+                user_msg = kwargs.get("user_msg", "")
+                df = kwargs.get("df", "")
                 #Verificar a possibilidade de relembrar que o agente é um assistente de análise de séries temporais, caso a resposta não seja satisfatória
                 #Vamos testar se listar as tools contribui para a resposta do agente 
-                prompt = "Agora foque em imputação de valores faltantes. Dada as séries temporais do dataframe df, verifique se há valores faltantes, caso haja, realize a imputação utilizando a tool mais apropriada para a série. Você tem as seguintes tools disponíveis: "
+                prompt = f"""Agora foque em imputação de valores faltantes.
+                Verifique se há valores faltantes em todo o dataframe.
+                Caso haja valores faltantes, realize a imputação utilizando a tool mais apropriada.
+                Use o formato: {{df.to_dict(), 'ETO'}} para qualquer tool de imputação."""
 
-            case 'Etapa 2': # Decomposição:
-                #Verificar a possibilidade de relembrar que o agente é um assistente de análise de séries temporais, caso a resposta não seja satisfatória
-                #Vamos testar se listar as tools contribui para a resposta do agente 
-                prompt = f"Sua tarefa é decidir se a Decomposição por Modos Empíricos (EMD) é benéfica para uma série temporal univariada. Para isso você deve verificar se a série é estacionária ou não, você tem a tool 'testar_estacionariedade' para fazer a verificação. Caso ela não seja estacionária, decomponha a série utilizando a tool 'EMD'. "
 
-            case 'Etapa 3': # Previsão:
+            case 'Etapa 2': # Previsão:
                 #verificar a possibilidade de tratamento de exceção caso o usuário não passe o nome da coluna 
-                target = kwargs.get("target", "")
-                step_ahead = kwargs.get("step_ahead", 5)
-                prompt = f"Faça uma previsão de {step_ahead} passos à frente da coluna {target} usando o dataframe df. O número máximo defasagens deve ser definido por você. Leve em consideração que quanto maior a entrada, maior será o custo computacional "
+                user_msg = kwargs.get("user_msg", "")
+                prompt = f"""Você é um assistente de previsão de séries temporais. Foque apenas na tarefa de previsão e solucione o problema: {user_msg}.
+                O número máximo de defasagens e o uso da decomposição deve ser definido por você.
+                O número máximo de defasagens deve ser definido de forma a equilibrar a precisão e o custo computacional, 
+                então escolha valores entre 1 e 30.
+                Para chamar a tool use o formato: {{target: 'nome da coluna', step_ahead: 5, max_lags: 15, decomposition: true}}, 
+                não use aspas nos nomes dos parâmetros.
+                """
 
-            case 'Etapa 4': # Explicação:
-                #POSSÍVEL EXEMPLO A USAR CASO O RESULTADO DA EXPLICAÇÃO ESTEJA RUIM
-                # Entrada : {coluna_alvo} = "OT"
-                # Saída de testar_estacionariedade: “A série temporal é provavelmente não estacionária.”
-                # Saída de EMD: DataFrame com 6 colunas.
-
-                # Resposta final esperada (resumo):
-
-                # A análise da série OT indicou, pelos testes ADF e KPSS, que ela é provavelmente não estacionária. Por esse motivo, o agente aplicou o método EMD, que é adequado para lidar com sinais não lineares e de frequência variável. 
-                state = kwargs.get("state", "")
-                tools_outputs = state.get("tool_output", "")
-                prompt = f"""A partir das saídas das ferramentas utilizadas, elabore um resumo explicativo que contenha os passos realizados pelo agente e as motivações para a realização desses passos. Exemplo: O
-                Saídas das ferramentas: {tools_outputs}
+            case 'Etapa 4': # Visualização real x previsto
+                target = state.get("target", "")
+                prompt = f'''Foque na tarefa de visualização de dados.
+                 Agora df contém os valores previstos e reais.
+                 Gere a figura na Base64 que contenha o gráfico comparando os valores reais e previstos da coluna {target}.
+                 
+                 '''
+                """
+            case 'Etapa 5': # Visualização grafo causal:
+                prompt = f"""Use a tool desenhar_grafo para gerar o grafo de importância das variáveis do modelo.
                 """
 
 
