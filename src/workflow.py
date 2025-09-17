d58155ebf65876513dbb738dc34a9e7a337e6dbb
Gabriel Ikaro <37417944+GabrielIkaro@users.noreply.github.com>
2025-09-17 09:33:07 -0300

    Update workflow.py

--- src/workflow.py
+++ src/workflow.py
@@ -5,12 +5,12 @@ from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
 from typing import List
 import pandas as pd
 from langchain_experimental.tools.python.tool import PythonAstREPLTool
-from langgraph.checkpoint.memory import MemorySaver
 import os
 from dotenv import load_dotenv 
 from typing import NotRequired
 import pickle
 import base64
+import json
 
 from tools import Tools
 import tools
@@ -33,179 +33,178 @@ tools_list = [Tools.automl, Tools.plot_real_vs_pred, Tools.testar_estacionarieda
 
 # Estados
 class State(TypedDict):
-    msg: List[BaseMessage]             # lista de mensagens do tipo HumanMessage ou AIMessage
+    msg: List[BaseMessage]  # lista de mensagens do tipo HumanMessage ou AIMessage
     step: NotRequired[int]             # etapa atual do workflow
     log: NotRequired[str]              # descrição da etapa executada (pensamento e ações do agente pandas)
-    tool_output: NotRequired[list]     # saída das tools executadas
-    resumo: NotRequired[list]          # histórico das etapas já resumidas. (list)
-    avaliacao: NotRequired[str]        # feedback do avaliador: sim ou não
+    tool_output: NotRequired[list]      # saída das tools executadas
+    resumo: NotRequired[list]           # histórico das etapas já resumidas. (list)
+    avaliacao: NotRequired[str]         # feedback do avaliador: sim ou não
     feedback: NotRequired[str]         # feedback do avaliador
-    dataframe: str                     # path do dataframe a ser analisado
+    dataframe: str        # path do dataframe a ser analisado
+    avaliador_count: int
 
 # Inicialização do dataframe e o modelo
 df = pd.DataFrame()
 modelo = None
 
-
 # Definição dos modelos e agentes
 model = "meta-llama/Llama-4-Maverick-17B-128E-Instruct-FP8"
-agente_pandas = Agent(model).build('pandas', df, tools_list)
-repl = next(t for t in agente_pandas.tools if isinstance(t, PythonAstREPLTool))
-
-# agente_react  = Agent(model).build('react', tools=tools_list)
+# agente_pandas = Agent(model).build('pandas', df, tools_list)
+# agente_react  = Agent(model).build('react')
 # agente_llm   = Agent(model).build('llm')
 
 # Implementação dos nós
 def executa_etapa(state: State):
-    global df, modelo, model, agente_pandas, repl
+    global df, modelo, model
     # Pega a etapa atual
     step = state.get("step", 1)
     print(f">>> Entrou no nó executa_etapa - step {step}", flush=True)
 
-    try:
-        if step == 1:
-            # Carregar o dataframe
-            df = pd.read_csv(state["dataframe"]).drop(columns=['Date']).head(2000)
-            df = utils.remover_valores_aleatorios(df, coluna="Power", proporcao=0.01)
-            repl.locals["df"] = df
-            tools.df = df
+    if step == 1:
 
-            # Pega o prompt da etapa 1 e gera a mensagem para o agente pandas
-            prompt = Prompts.get_prompt('Etapa 1')
-            messages = [HumanMessage(content=prompt)]
+        # Carregar o dataframe
+        df = pd.read_csv(state["dataframe"]).drop(columns=['Date']).head(5000)
+        df = utils.remover_valores_aleatorios(df, coluna="ETO", proporcao=0.1)
+        tools.df = df
 
-            # Executa o agente pandas e extrai os logs e outputs das tools
-            agent_output = agente_pandas.invoke(messages)
+        # Cria o agente pandas
+        agente_pandas = Agent(model).build('pandas', df, tools_list)
 
-            intermediate_steps = agent_output.get("intermediate_steps", [])
+        # Pega o prompt da etapa 1 e gera a mensagem para o agente pandas
+        prompt = Prompts.get_prompt('Etapa 1')
+        messages = [HumanMessage(content=prompt)]
 
-            logs = [action_log.log for action_log, _ in intermediate_steps]
-            
-            tool_outputs = {}
-            for action_log, observation in intermediate_steps:
-                tool_name = getattr(action_log, "tool", None)
-                output = utils.serialize_output(observation)
-                tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
-            
-            tool_output_final = next((v for k, v in tool_outputs.items() if "imput" in k), None)
-            novo_df = pd.DataFrame(tool_output_final)
-            
+        # Executa o agente pandas e extrai os logs e outputs das tools
+        agent_output = agente_pandas.invoke(messages)
 
-            # Atualiza o dataframe global e o dataframe nas tools
-            repl.locals["df"] = novo_df
-            df = novo_df
-            tools.df = novo_df
+        intermediate_steps = agent_output.get("intermediate_steps", [])
 
-            new_messages = state["msg"] + [AIMessage(content=logs)]
+        logs = [action_log.log for action_log, _ in intermediate_steps]
+        
+        tool_outputs = {}
+        for action_log, observation in intermediate_steps:
+            tool_name = getattr(action_log, "tool", None)
+            output = utils.serialize_output(observation)
+            tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
+        
+        tool_output_final = next((v for k, v in tool_outputs.items() if "imput" in k), None)
+        novo_df = pd.DataFrame(tool_output_final)
+
+        # Atualiza o dataframe global e o dataframe nas tools
+        df = novo_df
+        tools.df = df
+
+        new_messages = state["msg"] + [AIMessage(content=logs)]
 
 
-        elif step == 2:
+    elif step == 2:
+        # Cria o agente pandas
+        agente_pandas = Agent(model).build('pandas', df, tools_list)
 
-            # Pega a última mensagem humana para compor o prompt da etapa 2.
-            last_human_message = None
-            for msg in reversed(state["msg"]):
-                # caso 1: já é HumanMessage
-                if isinstance(msg, HumanMessage):
-                    last_human_message = msg.content
-                    break
-                # caso 2: veio como dict serializado
-                if isinstance(msg, dict) and msg.get("type") == "human":
-                    last_human_message = msg.get("content")
-                    break
+        # Pega a última mensagem humana para compor o prompt da etapa 2.
+        last_human_message = None
+        for msg in reversed(state["msg"]):
+            # caso 1: já é HumanMessage
+            if isinstance(msg, HumanMessage):
+                last_human_message = msg.content
+                break
+            # caso 2: veio como dict serializado
+            if isinstance(msg, dict) and msg.get("type") == "human":
+                last_human_message = msg.get("content")
+                break
 
-            # Pega o prompt da etapa 2 e gera a mensagem para o agente pandas
-            prompt = Prompts.get_prompt('Etapa 2', user_msg = last_human_message)
-            messages = [HumanMessage(content=prompt)]
+        # Pega o prompt da etapa 2 e gera a mensagem para o agente pandas
+        prompt = Prompts.get_prompt('Etapa 2', user_msg = last_human_message)
+        messages = [HumanMessage(content=prompt)]
 
-            # Executa o agente pandas e extrai os logs e outputs das tools
-            agent_output = agente_pandas.invoke(messages)
+        # Executa o agente pandas e extrai os logs e outputs das tools
+        agent_output = agente_pandas.invoke(messages)
 
-            intermediate_steps = agent_output.get("intermediate_steps", [])
+        intermediate_steps = agent_output.get("intermediate_steps", [])
 
-            logs = [action_log.log for action_log, _ in intermediate_steps]
-            
-            tool_outputs = {}
-            for action_log, observation in intermediate_steps:
-                tool_name = getattr(action_log, "tool", None)
-                output = utils.serialize_output(observation)
-                tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
-            
-            # Pega a saída da tool automl
-            dict_automl = next((v for k, v in tool_outputs.items() if "automl" in k), None)
-            modelo = pickle.loads(base64.b64decode(dict_automl['modelo']))
-            tool_output_final = dict_automl['predicoes']
-            novo_df = pd.DataFrame(dict_automl['predicoes'])
-            new_messages = state["msg"] + [AIMessage(content=logs)]
+        logs = [action_log.log for action_log, _ in intermediate_steps]
+        
+        tool_outputs = {}
+        for action_log, observation in intermediate_steps:
+            tool_name = getattr(action_log, "tool", None)
+            output = utils.serialize_output(observation)
+            tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
+        
+        # Pega a saída da tool automl
+        dict_automl = next((v for k, v in tool_outputs.items() if "automl" in k), None)
+        modelo = pickle.loads(base64.b64decode(dict_automl['modelo']))
+        tool_output_final = dict_automl['predicoes']
+        novo_df = pd.DataFrame(dict_automl['predicoes'])
 
-            # Para testar sem executar o automl
-            # tool_output_final = {'predicoes': {'real': {0: 5.8, 1: 5.5, 2: 5.8, 3: 5.8, 4: 5.6},
-            # 'previsto': {0: 5.086650089557475,
-            # 1: 4.981711405165194,
-            # 2: 4.960316273071086,
-            # 3: 4.714007595126947,
-            # 4: 4.5939236324712045}}}
-            # novo_df = pd.DataFrame(tool_output_final["predicoes"])
-            # modelo = pickle.load(open("model.pickle", 'rb'))
-            # logs = ""
-            # new_messages = state["msg"] + messages
-            
-            # Atualiza o dataframe global e o dataframe nas tools
-            repl.locals["df"] = novo_df
-            df = novo_df
-            tools.df = novo_df
-            tools.modelo = modelo
+        # Para testar sem executar o automl
+        # tool_output_final = {'predicoes': {'real': {0: 5.8, 1: 5.5, 2: 5.8, 3: 5.8, 4: 5.6},
+        # 'previsto': {0: 5.086650089557475,
+        # 1: 4.981711405165194,
+        # 2: 4.960316273071086,
+        # 3: 4.714007595126947,
+        # 4: 4.5939236324712045}}}
+        # novo_df = pd.DataFrame(tool_output_final["predicoes"])
+        # modelo = pickle.load(open("model.pickle", 'rb'))
+        # logs = ""
+        # new_messages = state["msg"] + messages
+        
+        # Atualiza o dataframe global e o dataframe nas tools
+        df = novo_df
+        tools.df = df
+        tools.modelo = modelo
 
-        elif step == 3:
+        new_messages = state["msg"] + [AIMessage(content=logs)]
 
-            # Pega o prompt da etapa 3 e gera a mensagem para o agente pandas
-            prompt = Prompts.get_prompt('Etapa 3')
-            messages = [HumanMessage(content=prompt)]
+    elif step == 3:
+        # Cria o agente pandas
+        agente_pandas = Agent(model).build('pandas', df, tools_list)
 
-            # Executa o agente pandas e extrai os logs e outputs das tools
-            agent_output = agente_pandas.invoke(messages)
+        # Pega o prompt da etapa 2 e gera a mensagem para o agente pandas
+        prompt = Prompts.get_prompt('Etapa 3')
+        messages = [HumanMessage(content=prompt)]
 
-            intermediate_steps = agent_output.get("intermediate_steps", [])
+        # Executa o agente pandas e extrai os logs e outputs das tools
+        agent_output = agente_pandas.invoke(messages)
 
-            logs = [action_log.log for action_log, _ in intermediate_steps]
-            
-            tool_outputs = {}
-            for action_log, observation in intermediate_steps:
-                tool_name = getattr(action_log, "tool", None)
-                output = utils.serialize_output(observation)
-                tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
-            
-            # Pega a saída da tool plot_real_vs_pred
-            tool_output_final = next((v for k, v in tool_outputs.items() if "plot_real_vs_pred" in k), None)
+        intermediate_steps = agent_output.get("intermediate_steps", [])
 
-            new_messages = state["msg"] + [AIMessage(content=logs)]
+        logs = [action_log.log for action_log, _ in intermediate_steps]
+        
+        tool_outputs = {}
+        for action_log, observation in intermediate_steps:
+            tool_name = getattr(action_log, "tool", None)
+            output = utils.serialize_output(observation)
+            tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
+        
+        # Pega a saída da tool plot_real_vs_pred
+        tool_output_final = next((v for k, v in tool_outputs.items() if "plot_real_vs_pred" in k), None)
 
-        elif step == 4:
+        new_messages = state["msg"] + [AIMessage(content=logs)]
 
-            # Pega o prompt da etapa 4 e gera a mensagem para o agente pandas
-            prompt = Prompts.get_prompt('Etapa 4', modelo = modelo)
-            messages = [HumanMessage(content=prompt)]
+    elif step == 4:
+        # Cria o agente pandas
+        agente_pandas = Agent(model).build('pandas', df, tools_list)
 
-            # Executa o agente pandas e extrai os logs e outputs das tools
-            agent_output = agente_pandas.invoke(messages)
+        # Pega o prompt da etapa 2 e gera a mensagem para o agente pandas
+        prompt = Prompts.get_prompt('Etapa 4', modelo = modelo)
+        messages = [HumanMessage(content=prompt)]
 
-            intermediate_steps = agent_output.get("intermediate_steps", [])
+        # Executa o agente pandas e extrai os logs e outputs das tools
+        agent_output = agente_pandas.invoke(messages)
 
-            logs = [action_log.log for action_log, _ in intermediate_steps]
-            
-            tool_outputs = {}
-            for action_log, observation in intermediate_steps:
-                tool_name = getattr(action_log, "tool", None)
-                output = utils.serialize_output(observation)
-                tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
-            
-            # Pega a saída da tool desenhar_grafo
-            tool_output_final = next((v for k, v in tool_outputs.items() if "desenhar_grafo" in k), None)
+        intermediate_steps = agent_output.get("intermediate_steps", [])
 
-            new_messages = state["msg"] + [AIMessage(content=logs)]
+        logs = [action_log.log for action_log, _ in intermediate_steps]
+        
+        tool_outputs = {}
+        for action_log, observation in intermediate_steps:
+            tool_name = getattr(action_log, "tool", None)
+            output = utils.serialize_output(observation)
+            tool_outputs[tool_name] = output   # chave = nome da tool, valor = output
+        
+        # Pega a saída da tool desenhar_grafo
+        tool_output_final = next((v for k, v in tool_outputs.items() if "desenhar_grafo" in k), None)
 
-    except Exception as e:
-        logs = f"Erro na execução da etapa {step}: {e}"
-        tool_output_final = ""
         new_messages = state["msg"] + [AIMessage(content=logs)]
 
     return {
@@ -215,8 +214,35 @@ def executa_etapa(state: State):
             }
 
 def avalia_etapa(state: State):
-    avaliacao = 'sim'
-    return {"avaliacao": avaliacao}
+    agente_resumidor = Agent(model)
+
+    prompt_avalia = Prompts.get_prompt('Avaliação')
+    out = agente_resumidor.llm.invoke(prompt_avalia)
+
+    json_out = json.loads(out.content)
+
+    avaliacao = json_out['avaliacao']
+    feedback = json_out['feedback']
+
+    if avaliacao == "não":
+        state['avaliador_count'] = state['avaliador_count'] + 1
+
+    if state['avaliador_count'] > 4:
+        avaliacao = "sim"
+        feedback = ""
+
+        state['avaliador_count'] = 0
+
+    print("Avaliação: " + avaliacao)
+    print("Feedback: " + feedback)
+    
+    state["avaliacao"] = avaliacao
+    state['feedback'] = feedback
+
+    if avaliacao == "sim":
+        state['feedback'] = ""
+    
+    return state
 
 def proxima_etapa(state: State):
     step = state.get("step") + 1
